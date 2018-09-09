6d2dca5161523cdcd8b6dd9a8124d946efed87a7
Zoe Sobin <zsobin@hubspot.com>
2018-09-09 15:47:27 -0400

    restructure to mimic HelloWorld

--- project/start.js
+++ project/start.js
@@ -3,10 +3,10 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/html');
 app.set('view engine', 'ejs');
 
 // Set up the app's routing
