3dd7d787580332def0afe4a417207f1de0071518
adin65 <46980135+adin65@users.noreply.github.com>
2020-09-06 22:59:59 -0400

    Style Index page, uploaded resume, responsive css.

--- project/js/main.js
+++ project/js/main.js
@@ -1,21 +1,27 @@
 $(document).ready(function() {
-    var headerHeight = $("header").outerHeight();
-    $(".content").css("margin-top", headerHeight);
+  var headerHeight = $("header").outerHeight();
+  adjustContentMargin();
 
   $(window).scroll(function () {
   // Get the offset position of the header
-    if ($(window).scrollTop() > 0) {
+    if (document.body.scrollTop > headerHeight/2 || document.documentElement.scrollTop > headerHeight/2) {
       $("header").css("opacity", 0.8);
+      $("header").css("padding", "0%");
+      $("header h1 a").css("font-size", '2vh');
     } else {
       $("header").css("opacity", 1);
+      $("header").css("padding", "");
+      $("header h1 a").css("font-size", '');
     }
   });
 
   $(window).resize(function(){
       //makes sure the context always have correct spacing
-    headerHeight = $("header").outerHeight();
-    $(".content").css("margin-top", headerHeight);
+    adjustContentMargin();
   });
     
-  
+  function adjustContentMargin() {
+    headerHeight = $("header").outerHeight();
+    $(".content").css("margin-top", headerHeight);
+  }
 });
